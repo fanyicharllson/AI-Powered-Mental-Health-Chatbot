86ec2c5ca7c10bba236e09c93936441be4a48070
fanyicharllson <fanyicharllson1@gmail.com>
2025-03-09 19:36:50 -0700

    Added welcome screen

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,11 @@
+import WelcomePage from "./components/WelcomePage";
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-red-500 flex items-center justify-center min-h-screen">
-      Welcome to CalmBo
-    </h1>
-  )
+    <main className="flex flex-col relative">
+       {/* Modern White & Blue Gradient Background */}
+       <div className="absolute inset-0 bg-gradient-to-br from-white via-blue-50 to-blue-200 z-0" />
+      <WelcomePage />
+    </main>
+  );
 }
