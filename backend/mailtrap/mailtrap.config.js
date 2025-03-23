e00b958bfbdb0cc52b726af22160b232c530fb50
fanyicharllson <fanyicharllson1@gmail.com>
2025-03-23 07:40:21 -0700

    Refactor Mailtrap configuration to use environment variable for token

--- backend/mailtrap/mailtrap.config.js
+++ backend/mailtrap/mailtrap.config.js
@@ -3,7 +3,7 @@ import { MailtrapClient } from "mailtrap";
 const TOKEN = "5b08af77442416361f63752bd44c34c6";
 
 export const mailtrapClient = new MailtrapClient({
-  token: '5b08af77442416361f63752bd44c34c6',
+  token: process.env.MAILTRAP_TOKEN || TOKEN,
 });
 
 export const sender = {
